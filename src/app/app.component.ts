8104aeb3e7703fb8d6b44820769d53112b888001
Alexander Oliveira <acc01719@sky.com.br>
2023-08-31 23:22:16 -0300

    Estudando os conceitos sobre Promise e Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
     return new Promise((resolve, reject) => {
       if(nome === 'Alexander'){
         setTimeout(() => {
-          resolve('Seja bem vindo ' + nome);
+          resolve('Seja Bem Vindo ' + nome);
         }, 1000);
       }
       else{
